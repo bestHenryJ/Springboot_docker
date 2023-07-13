6b1c956f908c71b49534a8853ba5b79983e25abf
Henry <hjnz1210@gmail.com>
2023-07-12 22:58:21 -0700

    test commit 2

--- src/main/java/com/example/springbootdocker/SpringbootDockerApplication.java
+++ src/main/java/com/example/springbootdocker/SpringbootDockerApplication.java
@@ -8,7 +8,6 @@ public class SpringbootDockerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDockerApplication.class, args);
-        System.out.println("Hello world");
     }
 
 }
